21eb9316b9abe9b459f80c90d2c41221673138c9
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-05-11 11:11:52 +0300

    set root view controller

--- Weather App/AppDelegate.swift
+++ Weather App/AppDelegate.swift
@@ -11,8 +11,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -34,4 +32,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
--- Weather App/LocationsViewController.swift
+++ Weather App/LocationsViewController.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LocationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
     }
 
-
 }
-
