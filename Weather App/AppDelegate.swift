ddb0d14ed9e2512f2beeebcd97f964dd5778b5cd
Sylvan Ash <sylvan.d.ash@gmail.com>
2020-05-11 12:30:01 +0300

    Set root controller for iOS 9

--- Weather App/AppDelegate.swift
+++ Weather App/AppDelegate.swift
@@ -11,13 +11,25 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 13.0, *) {
+            return true
+        }
+
+        let controller = LocationsViewController()
+        let navController = UINavigationController(rootViewController: controller)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
